f04e4ff56d45a1364c9d3cf0ef53392d562a2847
therealbeez <gabriel.agbese2@gmail.com>
2022-12-15 19:06:25 +0100

    layout structure completed. recpe card fix needed

--- src/App.js
+++ src/App.js
@@ -1,94 +1,116 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import MealList from "./MealList"
 import { useEffect } from "react";
 import "./layout.css"
+import gsap from "gsap"
+
 
 function App() {
-  const [mealData, setMealData] = useState(null);
-  const [ingredients, setIngredients] = useState("chicken,curry");
-  var [ingredientList, listChange] = useState([""])
-  let stringResult;
-  
-
-  useEffect(() => {
-    var numbs = 0;
-    //const ul = document.createElement('ul');
-    const ul = document.querySelector(".ingredient-track");
-
-    ul.innerHTML = ""
-    
-
-    ingredientList.forEach(item => {
-      let li = document.createElement('li');
-      li.classList.add("tagstyle");
-      ul.appendChild(li);
-      li.addEventListener('click', function() {
-        let filteredArray = ingredientList.filter(ingredients => ingredients !== item);
-        listChange(filteredArray);
-        console.log(item+" clicked")
-        console.log(ingredientList);
-      })
-
-      li.innerHTML += item;
-      numbs +=1
-      //console.log(numbs)
-    });
-    stringResult = String(ingredientList)
-    console.log(stringResult);
-
-    
-    //
-  },);
-
-  
-  
-
-  function handleChange(e){
-    setIngredients(e.target.value);
-    
-  }
-
-  function handleInput(e){
-    //list.push(ingredients);
-    listChange([...ingredientList,ingredients]);
-    
-    document.querySelector(".ingInput").value = ""
-    document.querySelector(".ingInput").focus();
-    
-  }
-
-  function getMealData(){
-    console.log("passed string: "+stringResult)
-    fetch(
-      'https://api.spoonacular.com/recipes/findByIngredients?apiKey=33ea468d6ee248d59f00052842c3332b&ingredients='+stringResult
-    )
-    .then((response)=> response.json())
-    .then((data) => {
-      setMealData(data);
-      console.log(data)
-    })
-    .catch(()=>{
-      console.log("error")
-    })
-  }
+	const [mealData, setMealData] = useState(null);
+	const [ingredients, setIngredients] = useState("chicken,curry");
+	var [ingredientList, listChange] = useState([])
+	let stringResult;
 
 
-  return (
-    <div className="App">
-      <ul className="ingredient-track"></ul>
-      <section className="controls">
-        <input 
-          type="string"
-          className="ingInput"
-          placeholder="ingredients"
-          onChange={handleChange}
-        />
-        <button onClick={handleInput}>+</button>
-        <button onClick={getMealData}>Get Recipe</button>
-        {mealData && <MealList mealData={mealData} />}
-      </section>
-    </div>
-  );
+	useEffect(() => {
+		var numbs = 0;
+		//const ul = document.createElement('ul');
+		const ul = document.querySelector(".ingredient-track");
+
+		ul.innerHTML = ""
+
+
+		ingredientList.forEach(item => {
+			let li = document.createElement('li');
+			li.classList.add("tagstyle");
+			ul.appendChild(li);
+			li.addEventListener('click', function () {
+				let filteredArray = ingredientList.filter(ingredients => ingredients !== item);
+				listChange(filteredArray);
+				console.log(item + " clicked")
+				console.log(ingredientList);
+			})
+
+			li.innerHTML += item;
+			numbs += 1
+			//console.log(numbs)
+		});
+		stringResult = String(ingredientList)
+		console.log(stringResult);
+
+		//
+	},);
+
+	function handleChange(e) {
+		setIngredients(e.target.value);
+
+	}
+
+	function handleInput(e) {
+		//list.push(ingredients);
+		listChange([...ingredientList, ingredients]);
+
+		document.querySelector(".ingInput").value = ""
+		document.querySelector(".ingInput").focus();
+
+	}
+
+	let tl = gsap.timeline()
+	let recipeList = document.querySelector(".result")
+
+	function showRecipeList(){
+		if(window.innerWidth < 720){
+			tl.to(recipeList,{yPercent: -100,duration: 2})
+		}
+	}
+
+	function hideRecipeList(){
+		if(window.innerWidth < 720){
+			tl.to(recipeList,{yPercent: 100,duration: 2})
+		}
+	}
+
+	function getMealData() {
+		console.log("passed string: " + stringResult)
+		fetch(
+			'https://api.spoonacular.com/recipes/findByIngredients?apiKey=33ea468d6ee248d59f00052842c3332b&ingredients=' + stringResult
+		)
+			.then((response) => response.json())
+			.then((data) => {
+				setMealData(data);
+				console.log(data)
+			})
+			.catch(() => {
+				console.log("error")
+			})
+		
+		showRecipeList();
+	}
+
+
+	return (
+		<div className="app">
+			<section className="controls">
+				<div className="input-section">
+					<div className="input-section-top">
+						<input
+							type="string"
+							className="ing-input"
+							placeholder="ingredients"
+							onChange={handleChange}
+						/>
+						<button className="add-button" onClick={handleInput}>+</button>
+					</div>
+					<button className="get-button" onClick={getMealData}>Get Recipe</button>
+				</div>
+				<ul className="ingredient-track"></ul>
+			</section>
+			<section className="result">
+				<p onClick={hideRecipeList}>X</p>
+				{mealData && <MealList mealData={mealData} />}
+			</section>
+		</div>
+	);
 }
 
 export default App;
--- src/Meal.js
+++ src/Meal.js
@@ -1,45 +1,65 @@
 import React from 'react'
-import {useState,useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import RecipeCard from './RecipeCard';
+import gsap from 'gsap';
 
 export default function Meal({ meal }) {
 
-    var mealinfo = null; 
-    const [recipeData, setRecipeData] = useState(null);
-    const [prepTime, setPrepTime] = useState(null);
-    const [servingsNo, setServingsNo] = useState(null);
+	var mealinfo = null;
+	const [recipeData, setRecipeData] = useState(null);
+	const [prepTime, setPrepTime] = useState(null);
+	const [servingsNo, setServingsNo] = useState(null);
 
-    function getDetails(id){
-        fetch(
-            // 'https://api.spoonacular.com/recipes/findByIngredients?apiKey=d711325cee4d4d2b969afb272193fcb5&ingredients='+ingredients
-            'https://api.spoonacular.com/recipes/'+id+'/information?apiKey=09c24c93b2974a7b86a966713897fa24&includeNutrition=false'
-          )
-          .then((response)=> response.json())
-          .then((data) => {
-            setRecipeData(data.instructions);
-            console.log(data.instructions)
-            setPrepTime(data.readyInMinutes)
-            setServingsNo(data.servings)
-          })
-          .catch(()=>{
-            console.log("error")
-          })
-    }
+	function getDetails(id) {
+		fetch(
+			// 'https://api.spoonacular.com/recipes/findByIngredients?apiKey=d711325cee4d4d2b969afb272193fcb5&ingredients='+ingredients
+			'https://api.spoonacular.com/recipes/' + id + '/information?apiKey=09c24c93b2974a7b86a966713897fa24&includeNutrition=false'
+		)
+			.then((response) => response.json())
+			.then((data) => {
+				setRecipeData(data.instructions);
+				console.log(data.instructions)
+				setPrepTime(data.readyInMinutes)
+				setServingsNo(data.servings)
+			})
+			.catch(() => {
+				console.log("error")
+			})
+	}
 
-    return (
-   <article>
-        <h1>{meal.title}</h1>
-        <img src={meal.image} alt="recipe" />
-        <ul>
-            <li>No of available ingredients used: {meal.usedIngredientCount}</li>
-            <li>No of missing ingredients: {meal.missedIngredientCount}</li>
-            {/* <li>Ingredients used: {meal.usedIngredients.name}</li> */}
-        </ul>
+	function seeRecipe(){
+		let tl2 = gsap.timeline();
+		let recipeWrapper = document.querySelector(".recipe-wrapper")
+		getDetails(meal.id);
+		tl2.fromTo(recipeWrapper,{xPercent: 0, opacity: 0,duration: 1},{xPercent: -100, opacity: 1,duration: 1})
+	}
 
-        {/* <a onClick={() => {getDetails(meal.id)}}>See Details</a>
-        <div> {mealinfo}</div> */}
-        <button onClick={() => {getDetails(meal.id)}}>See Recipe</button>
-        {recipeData && <RecipeCard recipeData={recipeData} prepTime={prepTime} servingsNo={servingsNo} />}
-   </article>
-  )
+	function recipeExit(){
+		let tl2 = gsap.timeline();
+		let recipeWrapper = document.querySelector(".recipe-wrapper")
+		getDetails(meal.id);
+		tl2.to(recipeWrapper,{xPercent: -100, opacity: 0,duration: 1})
+	}
+
+	return (
+		<section className='meal-list-wrapper'>
+			<article className='meal-list'>
+				<h1>{meal.title}</h1>
+				<img src={meal.image} alt="recipe" />
+				<ul>
+					<li>No of available ingredients used: {meal.usedIngredientCount}</li>
+					<li>No of missing ingredients: {meal.missedIngredientCount}</li>
+					{/* <li>Ingredients used: {meal.usedIngredients.name}</li> */}
+				</ul>
+
+				{/* <a onClick={() => {getDetails(meal.id)}}>See Details</a>
+        		<div> {mealinfo}</div> */}
+				<button onClick={() => { seeRecipe() }}>See Recipe</button>
+				
+			</article>
+			<div className='recipe-wrapper' onClick={() => {recipeExit()}}>
+				{recipeData && <RecipeCard recipeData={recipeData} prepTime={prepTime} servingsNo={servingsNo} />}
+			</div>
+		</section>
+	)
 }
